0fb6758a0aaf28e5265edce1e1aafc4b1986aa03
BastienKody <basguilla@gmail.com>
2023-08-19 21:25:53 +0200

    Revert "fix:clean repo"
    This reverts commit c8e8d1641f8358b03350e3958c7bd48c55a6fcd3.

---
+++ atoi/ft_atoi_novf.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_ll_nvof.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 17:46:31 by bguillau          #+#    #+#             */
+/*   Updated: 2023/08/09 17:46:33 by bguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+#include "../libft.h"
+
+/*	- atoi nptr into nb passed by reference
+	- nb is set to 0
+	- accepts single '+' or single '-'
+	- returns 0 if ovlf ; 1 if ok	*/
+
+int	ft_atoi_novf(const char *nptr, int *nb)
+{
+	int	sign;
+	int	prev_nb;
+
+	if (!ft_strcmp(nptr, INT_MIN_STR))
+		return (*nb = -2147483647 -1, 1);
+	sign = 1;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign *= -1;
+		nptr++;
+	}
+	*nb = 0;
+	prev_nb = 0;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		*nb = *nb * 10 + (*nptr++ - '0');
+		if (*nb < prev_nb)
+			return (0);
+		prev_nb = *nb;
+	}
+	*nb = *nb * sign;
+	return (1);
+}
